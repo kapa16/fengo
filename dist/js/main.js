90b7b4f32fcfa9dca666487c40500e547d99c3c8
Pavel <kapa16@inbox.ru>
2018-11-13 17:47:58 +0500

    do

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,66 +5,49 @@ function showMenu(classMenu) {
 };
 
 function hideMenu(classMenu, elem) {
-    classMenu.removeClass("menu-show");
+    classMenu.removeClass("display-flex");
 };
 
 $(document).ready(function () {
 
-//    $(".logo-brand").on("mouseenter", function () {
-//        showMenu(".main-logo-menu");
-//    });
-
-//    $(".browse").on("mouseenter", function () {
-//        showMenu(".dropdown-box");
-//    });
-
-    $(".main-menu__responsive-menu").on("click", function () {
+   $(".main-menu__responsive-menu").on("click", function () {
         showMenu(".main-menu__menu");
     });
 
     $(document).on("click", function (elem) {
-        hideMenu($(".main-logo-menu"), elem);
-        hideMenu($(".dropdown-box"), elem);
-        hideMenu($(".cart-menu"), elem);
+        hideMenu(".main-menu__menu");
     });
 
 
-    $(".slider-control-wrap").on("click", function () {
-        $(".slider-control-wrap .slider-control").removeClass("active");
-        $(".slider-control", this).addClass("active");
-    });
-    
-    //=============== sidebar ===============
-    $(function () {
-        var Accordion = function (el, multiple) {
-            this.el = el || {};
-            // more then one submenu open?
-            this.multiple = multiple || false;
-
-            var dropdownlink = this.el.find('.sidebar-title');
-            dropdownlink.on('click', {
-                    el: this.el,
-                    multiple: this.multiple
-                },
-                this.dropdown);
-        };
-
-        Accordion.prototype.dropdown = function (e) {
-            var $el = e.data.el,
-                $this = $(this),
-                //this is the ul.submenuItems
-                $next = $this.next();
-
-            $next.slideToggle();
-            $this.parent().toggleClass('menu-open');
-
-            if (!e.data.multiple) {
-                //show only one menu at the same time
-                $el.find('.sidebar-submenu').not($next).slideUp().parent().removeClass('menu-open');
-            }
-        }
-
-        var accordion = new Accordion($('.sidebar-menu'), false);
-    })
 
 });
+
+function changeClass(activeSlider, newSlider){
+    activeSlider.removeClass("display-flex");
+    newSlider.addClass("display-flex");
+};
+
+$(document).on("click", ".control-prev", function (elem) {
+    let slider = $(this).parent();
+    let activeSlider = slider.find(".display-flex");
+    let prevSlider = activeSlider.prev(".slider_block");
+
+    if (prevSlider.length == 0) {
+        prevSlider = activeSlider.nextAll(".slider_block").last();
+    }
+    changeClass(activeSlider, prevSlider);
+});
+
+$(document).on("click", ".control-next", function (elem) {
+    let slider = $(this).parent();
+    let activeSlider = slider.find(".display-flex");
+    let nextSlider = activeSlider.next(".slider_block");
+
+    if (nextSlider.length == 0) {
+        nextSlider = activeSlider.prevAll(".slider_block").last();
+    }
+
+    changeClass(activeSlider, nextSlider);
+});
+
+//--- todo сделать работу индикаторов слайдера
